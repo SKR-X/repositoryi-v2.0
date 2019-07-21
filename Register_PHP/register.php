59fff08b9f56e20a7b0f2df750d642648f27495a
SKR-X <42437146+SKR-X@users.noreply.github.com>
2019-07-22 00:31:59 +0300

    Update register.php

--- Register_PHP/register.php
+++ Register_PHP/register.php
@@ -13,7 +13,7 @@
    <div class="page">
 <div class="loginform">
   <div class="loginhdr"><h1>REGISTRATION</h1></div>
-<form method="POST" action="requiredfiles/register_user.php">
+<form method="POST" action="register_user.php">
 <input type="text" name="username" placeholder="Username" required>
 <input type="email" name="email" placeholder="E-mail" required>
 <input type="text" name="fio" placeholder="FIO" required>
