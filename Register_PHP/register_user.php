586d4dcc95b86f23889b1bcb699640206e76a445
SKR-X <42437146+SKR-X@users.noreply.github.com>
2019-07-29 18:18:32 +0300

    Update register_user.php

--- Register_PHP/register_user.php
+++ Register_PHP/register_user.php
@@ -3,7 +3,6 @@ require_once 'regclass.php';
 //set your own parameters
 $Reg = new Register(array('user' => '', 'pass' => '', 'db' => '', 'charset' => ''));
 $Reg->random_str(); 
-echo "Сгенерированный пароль: ";
-echo $Reg->done_pass;
-echo "<br>";
 $Reg->reg_user();
+echo "<br>"."Сгенерированный пароль: ";
+echo $Reg->done_pass;
