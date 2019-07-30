a23119547fc6fd2a6bebb7a2adce9e1a769bf92d
SKR-X <42437146+SKR-X@users.noreply.github.com>
2019-07-30 19:51:39 +0300

    Update register.php

--- Register_PHP/register.php
+++ Register_PHP/register.php
@@ -1,28 +1,31 @@
-<?php
-?>
 <!DOCTYPE html>
 <html>
- <head>
-   <title>Project</title>
-   <link rel="stylesheet" type="text/css" href="">
-   <meta name="viewport" content="width=device-width, initial-scale=1">
-   <meta charset="utf-8">
- </head>
- <body>
-   <div class="page">
-<div class="loginform">
-  <div class="loginhdr"><h1>REGISTRATION</h1></div>
-<form method="POST" action="register_user.php">
-<input type="text" name="username" placeholder="Username" required>
-<input type="email" name="email" placeholder="E-mail" required>
-<input type="text" name="fio" placeholder="FIO" required>
-<input type="text" name="country" placeholder="Country" required>
-<input type="text" name="region" placeholder="Region" required>
-<input type="text" name="city" placeholder="City" required>
-<input type="submit" name="reg" value="REGISTER" required>
-</form>
-<p>Password will be generated automatically</p>
-</div>
-</div>
-</body> 
+
+<head>
+    <title>Project</title>
+    <link rel="stylesheet" type="text/css" href="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+</head>
+
+<body>
+    <div class="page">
+        <div class="loginform">
+            <div class="loginhdr">
+                <h1>REGISTRATION</h1>
+            </div>
+            <form method="POST" action="register_user.php">
+                <input type="text" name="username" placeholder="Username" required>
+                <input type="email" name="email" placeholder="E-mail" required>
+                <input type="text" name="fio" placeholder="FIO" required>
+                <input type="text" name="country" placeholder="Country" required>
+                <input type="text" name="region" placeholder="Region" required>
+                <input type="text" name="city" placeholder="City" required>
+                <input type="submit" name="reg" value="REGISTER" required>
+            </form>
+            <p>Password will be generated automatically</p>
+        </div>
+    </div>
+</body>
+
 </html>
