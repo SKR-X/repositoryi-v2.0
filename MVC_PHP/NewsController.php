ef435eb532be8e46dc41ac3164de37cca3172404
SKR-X <42437146+SKR-X@users.noreply.github.com>
2019-07-31 20:12:11 +0300

    Add files via upload

---
+++ MVC_PHP/NewsController.php
@@ -0,0 +1,14 @@
+<?php
+
+class NewsController
+{
+    public function actionIndex(){
+        echo "Welcome to this page";
+        return true;
+    }
+    public function actionView(){
+        echo "Welcome to this page1";
+        return true;
+    }
+}
+
---
+++ MVC_PHP/index.php
@@ -0,0 +1,17 @@
+<?php
+
+//Вывод ошибок 0/1
+ini_set('display_errors',1);
+error_reporting(E_ALL);
+
+//Подключение файлов системы
+define('ROOT',$_SERVER['DOCUMENT_ROOT']);
+require_once (ROOT.'\components\Router.php');
+
+//Подключаемся к БД
+//оп, нихуя
+//Обращаемся к Роутеру
+
+Router::run();
+
+?>
