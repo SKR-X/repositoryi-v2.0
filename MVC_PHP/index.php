50fdd502942a011f23f874d7cc90af8d8b2b1a66
SKR-X <42437146+SKR-X@users.noreply.github.com>
2019-07-31 20:15:55 +0300

    Update index.php

--- MVC_PHP/index.php
+++ MVC_PHP/index.php
@@ -6,12 +6,10 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 
-//Подключение файлов системы
+//Подключение файлов системы (укажите свои пути)
 define('ROOT',$_SERVER['DOCUMENT_ROOT']);
 require_once (ROOT.'\components\Router.php');
 
-//Подключаемся к БД
-//оп, нихуя
 //Обращаемся к Роутеру
 
 Router::run();
